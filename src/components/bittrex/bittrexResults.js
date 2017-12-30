f38cdcfa786fa6c2118768516c4d36d3a61bb051
TobiahRex <tobiah.rex@gmail.com>
2017-12-29 23:29:49 -0700

    built BittrexResults component for rendering API call results.

--- src/components/bittrex/bittrexResults.js
+++ src/components/bittrex/bittrexResults.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { RaisedButton } from 'material-ui';
+import _ from 'lodash';
 
-const BittrexResults = ({ results }) => {
-  return (
-    <div>
-      {results}
-    </div>
-  );
-};
+class BittrexResults extends React.Component {
+  static propTypes = {
+    getMarketSummaries: PropTypes.func.isRequired,
+    results: PropTypes.objectOf(PropTypes.any),
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: '',
+    };
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    if (!_.isEqual(nextProps.results, this.props.results)) {
+      this.setState(() => ({
+        results: nextProps.results,
+      }));
+    }
+  }
+
+  getMarketSummaries = () => this.props.getMarketSummaries();
+
+  render() {
+    return (
+      <div>
+        <RaisedButton
+          primary
+          label={'Get Markets'}
+          type={'button'}
+          onClick={this.getMarketSummaries}
+        />
+        {this.state.results}
+      </div>
+    );
+  }
+}
 
 export default BittrexResults;
