e44eaa7ce86ea89a7223c35ce247beb148ef32d2
TobiahRex <tobiah.rex@gmail.com>
2018-01-01 22:13:51 -0700

    updated folioRebalance.js for testing.

--- server/db/bittrex/helpers/folioRebalance.js
+++ server/db/bittrex/helpers/folioRebalance.js
@@ -16,55 +16,6 @@ bittrexApi.options({
   inverse_callback_arguments: true,
 });
 
-const tokens = {
-  0: {
-    symbol: 'SALT',
-    contractAddress: '0x123123123123',
-    decimals: '18',
-    balance: '750',
-    percentage: '.2',
-    publicExgAddress: '0x123123123',
-    exchange: 'bittrex',
-    coldStorageAddress: '0x123123123',
-  },
-  1: {
-    symbol: 'ADA',
-    contractAddress: '0x123123123123',
-    balance: '1000',
-    percentage: '.2',
-    publicExgAddress: '0x123123123',
-    exchange: 'bittrex',
-    coldStorageAddress: '0x123123123',
-  },
-  2: {
-    symbol: 'BTC',
-    contractAddress: '0x123123123123',
-    balance: '15',
-    percentage: '.2',
-    publicExgAddress: '0x123123123',
-    exchange: 'bittrex',
-    coldStorageAddress: '0x123123123',
-  },
-  3: {
-    symbol: 'ETH',
-    contractAddress: '0x123123123123',
-    balance: '6',
-    percentage: '.2',
-    publicExgAddress: '0x123123123',
-    exchange: 'bittrex',
-    coldStorageAddress: '0x123123123',
-  },
-  4: {
-    symbol: 'BCH',
-    contractAddress: '0x123123123123',
-    balance: '5',
-    percentage: '.2',
-    publicExgAddress: '0x123123123',
-    exchange: 'bittrex',
-    coldStorageAddress: '0x123123123',
-  },
-};
-
 const getMarketSummary = market =>
 new Promise((resolve, reject) => {
   bbPromise.fromCallback(cb => bittrexApi.getmarketsummary({ market }, cb))
@@ -78,74 +29,114 @@ new Promise((resolve, reject) => {
   // Calculate current asset value compared to it's Bitcoin value and dollar value.
   // reduce the overall amount.
   // return result.
-  const assetReqs = assets.map(({  }))
 
-  return ({
-    SALT: {
-      value: '15.00',
-    },
-    BTC: {
-      value: '15000.00',
-    },
-    ETH: {
-      value: '700.00',
-    },
-    BCH: {
-      value: '3000.00',
-    },
-    ADA: {
-      value: '0.70',
-    },
-  });
+  Promise.all([
+    assets.map(({ symbol }) =>
+      getMarketSummary(`BTC-${symbol === 'BTC' ? 'USD' : symbol}`)
+    ),
+  ])
+  .then((results) => {
+    console.log('results: ', results);
+  })
+  .catch(reject);
+
+  // return ({
+  //   SALT: {
+  //     value: '15.00',
+  //   },
+  //   BTC: {
+  //     value: '15000.00',
+  //   },
+  //   ETH: {
+  //     value: '700.00',
+  //   },
+  //   BCH: {
+  //     value: '3000.00',
+  //   },
+  //   ADA: {
+  //     value: '0.70',
+  //   },
+  // });
 });
 
-function deposit(amount, folio) {
-  if (!amount && typeof amount !== 'object') throw Error('Invalid argument "amount".');
+function deposit(newAsset, assets) {
+  if (!newAsset && typeof newAsset !== 'object') throw Error('Invalid argument "newAsset".');
 
-  if (!folio && typeof folio !== 'object') throw Error('Invalid argument "amount".');
+  if (!assets && typeof assets !== 'object') throw Error('Invalid argument "assets".');
 
   /* Check current value of assets.
   - Fetch each Asset Price BTC-<Asset>
   - Fetch BTC-USD price.
   - Multiply the overall value by the folio settings.
   */
-  getAssetPrices(tokens)
-  .then((prices) => {
-    // Asset QTY => Asset Price (BTC) => All Assets(BTC) => BTC-USD => Final USD value.
-    const folioUsdValue = Object
-    .keys(tokens)
-    .reduce((key, n) => {
-      const token = tokens[key];
-      const usdToken = prices[token.symbol].value * token.balance;
-      return (n + usdToken);
-    }, 0);
-    // Divide USD value by desired folio Percentages.
-    return rebalanceFolio(newFolio);
-  })
-  .then((newTokens) => {
-    tokens = newTokens;
-  });
+  getAssetPrices(assets)
+  // .then((prices) => {
+  //   // Asset QTY => Asset Price (BTC) => All Assets(BTC) => BTC-USD => Final USD value.
+  //   const folioUsdValue = Object
+  //   .keys(tokens)
+  //   .reduce((key, n) => {
+  //     const token = tokens[key];
+  //     const usdToken = prices[token.symbol].value * token.balance;
+  //     return (n + usdToken);
+  //   }, 0);
+  //   // Divide USD value by desired folio Percentages.
+  //   return rebalanceFolio(newFolio);
+  // })
+  // .then((newTokens) => {
+  //   tokens = newTokens;
+  // });
 }
 
 console.log(
-  deposit({ amount: 1, asset: 'BTC', from: '000000000123' }, {
-    assets: [
+  deposit(
+    {
+      amount: 1,
+      asset: 'BTC',
+      from: '000000000123',
+    },
+    [
       {
         symbol: 'SALT',
-        percent: '.2',
+        contractAddress: '0x123123123123',
+        decimals: '18',
+        balance: '750',
+        percentage: '.2',
+        publicExgAddress: '0x123123123',
+        exchange: 'bittrex',
+        coldStorageAddress: '0x123123123',
       }, {
         symbol: 'ADA',
-        percent: '.2',
+        contractAddress: '0x123123123123',
+        balance: '1000',
+        percentage: '.2',
+        publicExgAddress: '0x123123123',
+        exchange: 'bittrex',
+        coldStorageAddress: '0x123123123',
       }, {
         symbol: 'BTC',
-        percent: '.2',
+        contractAddress: '0x123123123123',
+        balance: '15',
+        percentage: '.2',
+        publicExgAddress: '0x123123123',
+        exchange: 'bittrex',
+        coldStorageAddress: '0x123123123',
       }, {
         symbol: 'ETH',
-        percent: '.2',
+        contractAddress: '0x123123123123',
+        balance: '6',
+        percentage: '.2',
+        publicExgAddress: '0x123123123',
+        exchange: 'bittrex',
+        coldStorageAddress: '0x123123123',
       }, {
         symbol: 'BCH',
-        percent: '.2',
+        contractAddress: '0x123123123123',
+        balance: '5',
+        percentage: '.2',
+        publicExgAddress: '0x123123123',
+        exchange: 'bittrex',
+        coldStorageAddress: '0x123123123',
       },
     ],
-  },
-));
+  )
+);
