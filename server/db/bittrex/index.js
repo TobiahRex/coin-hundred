500ba2b0f046d1864ee17e873b1fd663d4bfd608
TobiahRex <tobiah.rex@gmail.com>
2018-01-01 21:43:34 -0700

    refactor works as intended.

--- server/db/bittrex/index.js
+++ server/db/bittrex/index.js
@@ -49,42 +49,6 @@ bittrexSchema.statics.getMarketSummaries = (cb) => {
     cb(null, data);
   })
   .catch(cb);
-  // bittrexApi.getmarkets((err, data) => {
-  //   if (err) cb(err);
-  //   else {
-  //     data.result.forEach(({
-  //       MarketCurrency,
-  //       MarketCurrencyLong,
-  //       LogoUrl,
-  //     }) => {
-  //       marketsMemo[MarketCurrency] = {
-  //         LogoUrl,
-  //         MarketCurrency,
-  //         MarketCurrencyLong,
-  //       };
-  //     });
-  //
-  //     bittrexApi.getmarketsummaries((err2, data2) => {
-  //       if (err2) cb(err2);
-  //       else {
-  //         data2.result = data2.result
-  //           .map(market => ({
-  //             ...market,
-  //             MarketCurrency: marketsMemo[
-  //               market.MarketName.split('-')[1]
-  //             ].MarketCurrency,
-  //             MarketCurrencyLong: marketsMemo[
-  //               market.MarketName.split('-')[1]
-  //             ].MarketCurrencyLong,
-  //             LogoUrl: marketsMemo[
-  //               market.MarketName.split('-')[1]
-  //             ].LogoUrl,
-  //           }));
-  //         cb(null, data2);
-  //       }
-  //     });
-  //   }
-  // });
 };
 
 bittrexSchema.statics.getMarketSummary = (market, cb) => {
