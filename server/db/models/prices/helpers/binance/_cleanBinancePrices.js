2160584822fe9cf88f365f1a36f569537d5b3857
TobiahRex <tobiah.rex@gmail.com>
2018-01-05 22:33:16 -0700

    refactoring logic for _cleanBinancePrices. - CONTD

--- server/db/models/prices/helpers/binance/_cleanBinancePrices.js
+++ server/db/models/prices/helpers/binance/_cleanBinancePrices.js
@@ -1,45 +1,4 @@
 /* eslint-disable import/prefer-default-export */
-export const _cleanBinancePrices = prices =>
-  Object.keys(prices)
-  .map((symbol) => {
-    const price = prices[symbol];
-
-    const btcStart = symbol.indexOf('BTC');
-    const ethStart = symbol.indexOf('ETH');
-    const usdtStart = symbol.indexOf('USDT');
-    const bnbStart = symbol.indexOf('BNB');
-    let cleanSymbol = '';
-
-    if (btcStart) {
-      if (btcStart > 0) {
-        cleanSymbol = `${symbol.slice(0, btcStart)}-BTC`;
-      } else {
-        cleanSymbol = `BTC-${symbol.slice(3)}`;
-      }
-    } else if (ethStart) {
-      if (ethStart > 0) {
-        cleanSymbol = `${symbol.slice(0, ethStart)}-ETH`;
-      } else {
-        cleanSymbol = `ETH-${symbol.slice(3)}`;
-      }
-    } else if (usdtStart) {
-      if (usdtStart > 0) {
-        cleanSymbol = `${symbol.slice(0, usdtStart)}-USDT`;
-      } else {
-        cleanSymbol = `USDT-${symbol.slice(3)}`;
-      }
-    } else if (bnbStart) {
-      if (bnbStart > 0) {
-        cleanSymbol = `${symbol.slice(0, bnbStart)}-BNB`;
-      } else {
-        cleanSymbol = `BNB-${symbol.slice(3)}`;
-      }
-    }
-    return ({
-      symbol: cleanSymbol,
-      last: price,
-    });
-  });
 
 const detectMajor = (major, symbol, prices) => {
   const price = prices[symbol];
@@ -60,9 +19,55 @@ const detectMajor = (major, symbol, prices) => {
   });
 };
 
-const funcs = [
-  detectMajor,
-  'detectBTC',
-  'detectBNB',
-  'detectUSDT',
-];
+export const _cleanBinancePrices = prices =>
+  Object.keys(prices)
+  .map((symbol) => {
+    const majors = [
+      'ETH',
+      'BTC',
+      'BNB',
+      'USDT',
+    ];
+    .forEach((major) => detectMajor(major, symbol, prices))
+  });
+// export const _cleanBinancePrices = prices =>
+//   Object.keys(prices)
+//   .map((symbol) => {
+//     const price = prices[symbol];
+//
+//     const btcStart = symbol.indexOf('BTC');
+//     const ethStart = symbol.indexOf('ETH');
+//     const usdtStart = symbol.indexOf('USDT');
+//     const bnbStart = symbol.indexOf('BNB');
+//     let cleanSymbol = '';
+//
+//     if (btcStart) {
+//       if (btcStart > 0) {
+//         cleanSymbol = `${symbol.slice(0, btcStart)}-BTC`;
+//       } else {
+//         cleanSymbol = `BTC-${symbol.slice(3)}`;
+//       }
+//     } else if (ethStart) {
+//       if (ethStart > 0) {
+//         cleanSymbol = `${symbol.slice(0, ethStart)}-ETH`;
+//       } else {
+//         cleanSymbol = `ETH-${symbol.slice(3)}`;
+//       }
+//     } else if (usdtStart) {
+//       if (usdtStart > 0) {
+//         cleanSymbol = `${symbol.slice(0, usdtStart)}-USDT`;
+//       } else {
+//         cleanSymbol = `USDT-${symbol.slice(3)}`;
+//       }
+//     } else if (bnbStart) {
+//       if (bnbStart > 0) {
+//         cleanSymbol = `${symbol.slice(0, bnbStart)}-BNB`;
+//       } else {
+//         cleanSymbol = `BNB-${symbol.slice(3)}`;
+//       }
+//     }
+//     return ({
+//       symbol: cleanSymbol,
+//       last: price,
+//     });
+//   });
