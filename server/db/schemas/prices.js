1247ef11583df7540a8fd38830e0bf5ef2c04465
TobiahRex <tobiah.rex@gmail.com>
2018-01-05 20:53:59 -0700

    created basic pricesSchema.

--- server/db/schemas/prices.js
+++ server/db/schemas/prices.js
@@ -1,1 +1,21 @@
+/* eslint-disable import/prefer-default-export */
 import mongoose from 'mongoose';
+
+const pricesSchema = new mongoose.Schema({
+  symbol: String,
+  marketName: String,
+  exchanges: {
+    bittrex: {
+      last: String,
+      symbol: String,
+      date: String,
+    },
+    binance: {
+      last: String,
+      symbol: String,
+      date: String,
+    },
+  },
+});
+
+export const Prices = mongoose.model('Prices', pricesSchema);
