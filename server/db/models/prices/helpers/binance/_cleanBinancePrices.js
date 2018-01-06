44bb3c8e6528813c3920e6c58f579dc9c55d47cb
TobiahRex <tobiah.rex@gmail.com>
2018-01-05 22:11:22 -0700

    testing prelim _cleanBinan Prices.

--- server/db/models/prices/helpers/binance/_cleanBinancePrices.js
+++ server/db/models/prices/helpers/binance/_cleanBinancePrices.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-const symbol = [
+const symbols = [
   '123456',
   'ETHBTC',
   'LTCBTC',
@@ -17,14 +17,21 @@ const symbol = [
   'HSRBTC',
 ];
 
-const _cleanBinancePrices = prices => {
+const _cleanBinancePrices = (prices) => {
   prices.map((symbol) => {
-    const iStart = symbol.lastIndexOf('BTC' || 'ETH');
-    symbol
+    const btcStart = symbol.indexOf('BTC');
+    let mysterySym = '';
 
-
-  })
+    if (btcStart) {
+      if (btcStart > 0) {
+        mysterySym = `${symbol.slice(0, btcStart)}-BTC`;
+      } else {
+        mysterySym = `BTC-${symbol.slice(3)}`;
+      }
+    }
+    return mysterySym;
+  });
   // return Object.keys(prices).map(symbolKey => {});
 };
 
-console.log(_cleanBinancePrices(symbol));
+console.log(_cleanBinancePrices(symbols));
