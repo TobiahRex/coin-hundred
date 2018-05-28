910e6c90b4eaa9de943b47cc8b274edc94a9c84a
Tobiah Rex <tobiah.rex@gmail.com>
2018-05-28 16:23:17 -0700

    finished building "getPrices" and sending response.

--- server/db/models/markets/index.js
+++ server/db/models/markets/index.js
@@ -18,7 +18,8 @@ bittrexApi.options({
   inverse_callback_arguments: true,
 });
 
-marketsSchema.statics.getPrices = () => {
+marketsSchema.statics.getPrices = () =>
+new Promise((resolve, reject) => {
   Promise.all([
     _getBinancePrices(),
     _getBittrexPrices(),
@@ -29,13 +30,9 @@ marketsSchema.statics.getPrices = () => {
       bittrex: _cleanBittrexPrices(prices[1].result),
     },
   }))
-  .then((result) => {
-    console.log('success.');
-  })
-  .catch((err) => {
-    console.log('err: ', err);
-  });
-};
+  .then(resolve)
+  .catch(reject);
+})
 
 marketsSchema.statics.createMarket = marketObj =>
   new Promise((resolve, reject) => {
@@ -46,27 +43,25 @@ marketsSchema.statics.createMarket = marketObj =>
       Markets.create(marketObj, cb)
     )
     .then((newMarket) => {
-      console.log('newMarket: ', newMarket);
-      if ('_id' in newMarket) resolve();
+      if ('_id' in newMarket) resolve(newMarket);
       else reject('FAILED: @func "createMarket".');
     })
     .catch(reject);
   });
 
-marketsSchema.statics.findMarketAndUpdate = marketObj =>
+marketsSchema.statics.updateMarket = marketObj =>
   new Promise((resolve, reject) => {
-    if (!('symbol' in marketObj)) reject('FAILED: @Markets.findMarketAndUpdate - Must supply required param "marketObj".');
-    if (marketObj && typeof marketObj !== 'object') reject('FAILED:  @Markets.findMarketAndUpdate - Must supply an {object} for param "marketObj".');
+    if (!('symbol' in marketObj)) reject('FAILED: @Markets.updateMarket - Must supply required param "marketObj".');
+    if (marketObj && typeof marketObj !== 'object') reject('FAILED:  @Markets.updateMarket - Must supply an {object} for param "marketObj".');
 
     bbPromise.fromCallback(cb =>
       Markets
       .findOne({ symbol: marketObj.symbol }, cb)
-      .exec()
       .then((dbMarket) => { // eslint-disable-line consistent-return
         if (!('_id' in dbMarket)) {
           reject(`FAILED: Could not find market to update: "${marketObj.symbol}"`);
         } else {
-          dbMarket.last = marketObj.last;
+          dbMarket.last = String(marketObj.last);
           dbMarket.timeStamp = marketObj.timeStamp;
           dbMarket.exchange = marketObj.exchange;
 
@@ -128,13 +123,16 @@ new Promise((resolve, reject) => {
   ])
   .then((results) => {
     // results = array of markets and db lookup status.
-    const createOrUpdateReqs = results.map(({ result, market }) => {
+    const reqCollection = results.map(({ result, market }) => {
       if (result) return Markets.updateMarket(market);
       return Markets.createMarket(market);
     });
-    return Promise.all(createOrUpdateReqs);
+    return Promise.all(reqCollection);
   })
-  .then(resolve)
+  .then((result) => {
+    console.log('result: ', result);
+    resolve();
+  })
   .catch(reject);
 });
 
